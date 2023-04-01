1329f6eddd9de9bf76d3fcd1b15cd09de1342500
Md. Asaduzzaman <bd.asad.bd@gmail.com>
2023-04-01 15:23:18 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,157 @@
+;
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function () {
+
+        // == AOS Init== //
+        AOS.init({
+            disable: 'mobile'
+        });
+
+        // == Search Bar== //
+        if ($('.search-icon').length) {
+            $('.search-icon').on('click', function () {
+                $('.search-form').toggleClass('show');
+            });
+        }
+
+        // == Hero Slider== //
+        if ($('.hero-slider').length) {
+            var swiper = new Swiper('.hero-slider', {
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: true,
+                },
+                speed: 900,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.arr-right',
+                    prevEl: '.arr-left',
+                },
+                on: {
+                    slideChangeTransitionStart: function () {
+                        $('.slide-content h1, .slide-content p, .slide-content a').removeClass('aos-init').removeClass('aos-animate');
+                    },
+                    slideChangeTransitionEnd: function () {
+                        AOS.init();
+                    },
+                },
+            });
+
+            $(".hero-slider").hover(function () {
+                (this).swiper.autoplay.stop();
+            }, function () {
+                (this).swiper.autoplay.start();
+            });
+        }
+
+        // == Testimonial Slider== //
+        if ($('.test-slider').length) {
+            var swiper = new Swiper('.test-slider', {
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: true,
+                },
+                speed: 1200,
+                loop: true,
+                pagination: {
+                    el: '.test-pagination',
+                    clickable: true
+                },
+                on: {
+                    slideChangeTransitionStart: function () {
+                        $('.testimonials .test-img, .testimonials h5, .testimonials span, .testimonials p').removeClass('aos-init').removeClass('aos-animate');
+                    },
+                    slideChangeTransitionEnd: function () {
+                        AOS.init();
+                    },
+                },
+            });
+
+            $(".test-slider").hover(function () {
+                (this).swiper.autoplay.stop();
+            }, function () {
+                (this).swiper.autoplay.start();
+            });
+        }
+
+        // == Clients Slider== //
+        if ($('.clients-slider').length) {
+            var swiper = new Swiper('.clients-slider', {
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: true,
+                },
+                speed: 900,
+                loop: true,
+                slidesPerView: 5,
+                breakpoints: {
+                    1200: {
+                        slidesPerView: 4
+                    },
+                    992: {
+                        slidesPerView: 3
+                    },
+                    576: {
+                        slidesPerView: 2
+                    },
+                    400: {
+                        slidesPerView: 1
+                    }
+                }
+            });
+
+            $(".clients-slider").hover(function () {
+                (this).swiper.autoplay.stop();
+            }, function () {
+                (this).swiper.autoplay.start();
+            });
+        }
+
+        // == Light Gallery== //
+        if ($('#lightgallery').length) {
+            $("#lightgallery").lightGallery();
+        }
+    });
+
+    jQuery(window).on('load', function () {
+        // == Animate loader off screen == //
+        $(".css-loader").fadeOut("slow");
+        AOS.init({
+            disable: 'mobile'
+        });
+
+    });
+})(jQuery)
+
+
+
+
+
+$(document).ready(function() {
+  // Add smooth scrolling to all links with the "scroll" class
+  $(".nav-link").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
